6b06da7d00e709a81bf05ab6f9e8310a1cdb7eb5
shashank7200 <shashank7200@gmail.com>
2017-09-10 18:46:11 +0530

    added upvote, downvote and delete scenarios for a post

--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
 class Posts extends Component {
+
+  handleUpvote = (post, key) => {
+    this.props.firebase.ref('posts/' + key).set({
+      title: post.title,
+      upvote: post.upvote + 1,
+      downvote: post.downvote
+    });
+  }
+  
+  handleDownvote = (post, key) => {
+    this.props.firebase.ref('posts/' + key).set({
+      title: post.title,
+      upvote: post.upvote,
+      downvote: post.downvote + 1
+    });
+  }
+
+  handleDelete = (post, key) => {
+    this.props.firebase.ref('posts/' + key).remove();
+  }
+
   render() {
     let {posts} = this.props;
+    const _this = this;
 
     if (this.props.loading) {
       return (
@@ -17,8 +39,32 @@ class Posts extends Component {
         { Object.keys(posts).map(function(key) {
             return (
               <div key={key}>
-                { posts[key].title }
+              <div>Title: { posts[key].title }</div>
+              <div>Upvotes: { posts[key].upvote }</div>
+              <div>Downvotes: { posts[key].downvote }</div>
+              <div>
+                <button 
+                  onClick={ _this.handleUpvote.bind(this, posts[key], key) }
+                  type="button"
+                >
+                  Upvote
+                </button>
+                <button 
+                  onClick={ _this.handleDownvote.bind(this, posts[key], key) }
+                  type="button"
+                >
+                  Downvote
+                </button>
+                
+                <button 
+                onClick={ _this.handleDelete.bind(this, posts[key], key) }
+                type="button"
+              >
+                Delete Post
+              </button>
+
               </div>
+            </div>
             );
         })}
       </div>
