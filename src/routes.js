ca52f59afc2204a660b15b8aa850a9add0021e02
shashank7200 <shashank7200@gmail.com>
2017-09-10 18:10:47 +0530

    added AddPost route

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Router, Route } from "react-router";
 
 import App from "./containers/App/index";
 import Posts from "./containers/Posts/index";
+import AddPost from "./containers/AddPost/index";
 
 const Routes = props => (
   <Router {...props}>
     <Route path="/" component={App}>
       <Route path="/posts" component={Posts} />
+      <Route path="/add-post" component={AddPost} />
     </Route>
   </Router>
 );
