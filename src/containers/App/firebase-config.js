e691447c46f7289c1d8f0270109e9fa39ee1d393
shashank7200 <shashank7200@gmail.com>
2017-09-10 17:36:47 +0530

    added firebase configuration

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBGxM2ddJ-1UsvPzllfdEw8cZ9JksjJMnE",
+  authDomain: "reddit-clone-bb6cd.firebaseapp.com",
+  databaseURL: "https://reddit-clone-bb6cd.firebaseio.com",
+  projectId: "reddit-clone-bb6cd",
+  storageBucket: "reddit-clone-bb6cd.appspot.com",
+  messagingSenderId: "568863562204"
+};
