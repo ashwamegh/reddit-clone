416e934364e28fafb12e7138f71955586d818eca
shashank7200 <shashank7200@gmail.com>
2017-09-10 17:37:51 +0530

    renamed app.js and created a cloneElement call to inject props to all nested elements

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import * as firebase from "firebase";
+
+import config from "./firebase-config";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      loading: true
+    };
+
+    // Initializes firebase
+    firebase.initializeApp(config);
+  }
+
+  componentWillMount() {
+    let postsRef = firebase.database().ref("posts");
+
+    let _this = this;
+
+    postsRef.on("value", snapshot => {
+      console.log(snapshot.val());
+
+      _this.setState({
+        posts: snapshot.val(),
+        loading: false
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        Hello World
+        {this.props.children &&
+          React.cloneElement(this.props.children, {
+            firebaseRef: firebase.database().ref("posts"),
+            posts: this.state.posts,
+            loading: this.state.loading
+          })}
       </div>
     );
   }
